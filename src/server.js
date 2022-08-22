57f79a5a2e83210772b2d19b636ba6df6c05b56f
Lucas Tavares <zlucas.np@gmail.com>
2022-08-22 17:56:22 -0300

    feat: feat: configura aplicação para usar hbs e testa rota raiz

--- src/server.js
+++ src/server.js
@@ -1,5 +1,24 @@
 const express = require('express');
+const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
 
 module.exports = app;
+
+app.set('views', path.join(__dirname, 'views'))
+app.engine('.hbs', hbs.engine({
+    defaultLayout: 'main',
+    layoutDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+
+app.use(express.urlencoded({extended: false}))
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (request, response) => {
+    response.render('index');
+})
