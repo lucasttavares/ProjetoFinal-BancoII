e673fe75e34b3c0032e2666a1869403139fb4724
Lucas Tavares <zlucas.np@gmail.com>
2022-08-23 16:28:53 -0300

    refactor

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -9,11 +9,11 @@ const {
     deleteForm
 } = require('../controllers/notesController')
 
-router.get('/notes', getNotes)
-router.get('/notes/add', getForm)
-router.post('/notes/add', postNote)
-router.get('/notes/edit/:id', getEditForm)
-router.put('/notes/edit/:id', editForm)
-router.delete('/notes/delete/:id', deleteForm)
+router.get('/notes', getNotes);
+router.get('/notes/add', getForm);
+router.post('/notes/addNote', postNote);
+router.get('/notes/edit/:id', getEditForm);
+router.put('/notes/edit/:id', editForm);
+router.delete('/notes/delete/:id', deleteForm);
 
 module.exports = router;
