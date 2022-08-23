0ef19477a544c2b82b2ce67879760e91661baa66
Lucas Tavares <zlucas.np@gmail.com>
2022-08-23 16:31:18 -0300

    feat: cria função de salvar dados no banco

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -1,15 +1,19 @@
 const notesCtrl = {}
+const Note = require('../models/Notes');
 
 notesCtrl.getNotes = (request, response) => {
     response.send('getNotes')
 }
 
 notesCtrl.getForm = (request,response) => {
-    response.send('getForm')
+    response.render('notes/addNote')
 }
 
-notesCtrl.postNote = (request,response) => {
-    response.send('postNote')
+notesCtrl.postNote = async (request,response) => {
+    const { titulo, descricao, data, tipo } = request.body
+    const newNote = new Note({titulo, descricao, data, tipo})
+    await newNote.save();
+    response.send('Nota salva com sucesso')
 }
 
 notesCtrl.getEditForm = (request,response) => {
