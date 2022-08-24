a8fafe0f0e3eed9c2fb7531c61b02a21009cedf9
Lucas Tavares <zlucas.np@gmail.com>
2022-08-24 20:37:48 -0300

    refactor: cria pasta de bancos para melhorar organização

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const app = require('./server');
-require('./db');
+require('./database/db');
 
 app.listen(process.env.API_PORT, ()=>{
     console.log("App rodando na porta " + process.env.API_PORT)
